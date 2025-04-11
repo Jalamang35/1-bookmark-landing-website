32825d793dc89ae09c69a4d79c001929921543af
Jalamang35 <jalamangsanyang35@gmail.com>
2025-04-11 02:12:19 +0000

    Fix import issue

--- src/main.js
+++ src/main.js
@@ -1,93 +1,88 @@
-// import './style.css'
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgarIcon = document.querySelector("#hamburgar-icon");
+  const desktopLogo = document.querySelector("#desktop-logo");
+  const mobileMenu = document.querySelector("#mobile-menu");
+  const closeIcon = document.querySelector("#close-icon");
 
-
-// HAMBURGER AND CLOSE MENU SECTION 
-const hamburgarIcon = document.querySelector("#hamburgar-icon");
-const desktopLogo = document.querySelector("#desktop-logo");
-const mobileMenu = document.querySelector("#mobile-menu");
-const closeIcon = document.querySelector("#close-icon");
-        
-hamburgarIcon.addEventListener("click", () => {
-  hamburgarIcon.classList.add("hidden");
-  desktopLogo.classList.add("hidden");
-  mobileMenu.classList.remove("hide");
-});
-
-closeIcon.addEventListener("click", () => {
-  hamburgarIcon.classList.remove("hidden");
-  desktopLogo.classList.remove("hidden");
-  mobileMenu.classList.add("hide");
-});
-
-
-// TABS AND ACTIVE INDICATOR MANIPULATION SECTION
-const tabs = document.querySelectorAll(".feature-tab");
-const sections = document.querySelectorAll(".feature-section");
-const activeIndicator = document.getElementById("active-indicator");
-
-tabs.forEach((tab, index) => {
-  tab.addEventListener("click", function () {
-    sections.forEach(section => section.classList.add("hide"));
-    sections[index].classList.remove("hide");
-
-    if (window.innerWidth >= 768) {
-      // Large screen (horizontal tabs)
-      activeIndicator.style.cssText = `
-        left: ${this.offsetLeft}px;
-        top: auto;
-        width: ${this.offsetWidth}px;
-        height: 2px;
-        transform: translateY(0);
-      `;
-    } else {
-      // Small screen (vertical tabs)
-      const offsetTop = this.parentElement.offsetTop + this.offsetTop + this.offsetHeight;
-      activeIndicator.style.cssText = `
-        left: 30%;
-        top: ${this.offsetTop + this.offsetHeight}px;
-        width: 40%;
-        height: 4px;
-        transform: translateY(-4px);
-      `;
-    }
+  hamburgarIcon.addEventListener("click", () => {
+    hamburgarIcon.classList.add("hidden");
+    desktopLogo.classList.add("hidden");
+    mobileMenu.classList.remove("hide");
   });
-});
 
+  closeIcon.addEventListener("click", () => {
+    hamburgarIcon.classList.remove("hidden");
+    desktopLogo.classList.remove("hidden");
+    mobileMenu.classList.add("hide");
+  });
 
-// FAQS SECTIONS
-const faqs = document.querySelectorAll(".faqs-container");
-  
-faqs.forEach((faq) => {
-  const arrowDown = faq.querySelector(".arrow-down");
-  const answer = faq.querySelector(".answer");
-  
-  faq.addEventListener("click", function () {
-    if (answer.classList.contains("hidden")) {
+  // TABS AND ACTIVE INDICATOR MANIPULATION SECTION
+  const tabs = document.querySelectorAll(".feature-tab");
+  const sections = document.querySelectorAll(".feature-section");
+  const activeIndicator = document.getElementById("active-indicator");
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", function () {
+      sections.forEach((section) => section.classList.add("hide"));
+      sections[index].classList.remove("hide");
+
+      if (window.innerWidth >= 768) {
+        // Large screen (horizontal tabs)
+        activeIndicator.style.cssText = `
+          left: ${this.offsetLeft}px;
+          top: auto;
+          width: ${this.offsetWidth}px;
+          height: 2px;
+          transform: translateY(0);
+        `;
+      } else {
+        // Small screen (vertical tabs)
+        const offsetTop =
+          this.parentElement.offsetTop + this.offsetTop + this.offsetHeight;
+        activeIndicator.style.cssText = `
+          left: 30%;
+          top: ${this.offsetTop + this.offsetHeight}px;
+          width: 40%;
+          height: 4px;
+          transform: translateY(-4px);
+        `;
+      }
+    });
+  });
+
+  // FAQS SECTIONS
+  const faqs = document.querySelectorAll(".faqs-container");
+
+  faqs.forEach((faq) => {
+    const arrowDown = faq.querySelector(".arrow-down");
+    const answer = faq.querySelector(".answer");
+
+    faq.addEventListener("click", function () {
+      if (answer.classList.contains("hidden")) {
         answer.classList.remove("hidden"); // Show the answer
-        arrowDown.style.cssText = `transform: rotate(180deg); filter: brightness(0) saturate(100%) invert(14%) sepia(100%) saturate(5000%) hue-rotate(0deg);`
-    } else {
-      answer.classList.add("hidden"); // Hide the answer
-      arrowDown.style.cssText = `transform: rotate(0deg);`
-    }
+        arrowDown.style.cssText = `transform: rotate(180deg); filter: brightness(0) saturate(100%) invert(14%) sepia(100%) saturate(5000%) hue-rotate(0deg);`;
+      } else {
+        answer.classList.add("hidden"); // Hide the answer
+        arrowDown.style.cssText = `transform: rotate(0deg);`;
+      }
+    });
   });
-});
-    
 
-// FORM VALIDIATION
-const form = document.querySelector("form");
-const emailInput = document.querySelector("#email");
-const submitButton = document.querySelector("#submit");
-const emailError = document.querySelector(".email-error");
-const emailInputGroup = document.querySelector(".input-group")
-const placeholder = document.querySelector("placeholder")
-const iconError = document.querySelector("#icon-error")
+  // FORM VALIDIATION
+  const form = document.querySelector("form");
+  const emailInput = document.querySelector("#email");
+  const submitButton = document.querySelector("#submit");
+  const emailError = document.querySelector(".email-error");
+  const emailInputGroup = document.querySelector(".input-group");
+  const placeholder = document.querySelector("placeholder");
+  const iconError = document.querySelector("#icon-error");
 
-form.addEventListener("submit", (event) => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
-  
+
     const emailValue = emailInput.value.trim();
     const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue);
-  
+
     if (!emailValue || !isValidEmail) {
       emailInput.value = "";
       emailInput.placeholder = "example@email/com";
@@ -95,22 +90,22 @@ form.addEventListener("submit", (event) => {
       submitButton.classList.add("submit-button-error");
       emailError.classList.remove("hide");
       emailInputGroup.classList.add("input-group-error");
-      iconError.classList.remove("hide")
+      iconError.classList.remove("hide");
     } else {
       // console.log("Valid Email");
     }
   });
-  
+
   emailInput.addEventListener("input", () => {
-    emailInput.iconError.classList.add("hide")
-    iconError.classList.add("hide")
+    emailInput.iconError.classList.add("hide");
+    iconError.classList.add("hide");
     emailInput.placeholder = "Enter your email address";
     submitButton.classList.remove("submit-button-error");
     emailError.classList.add("hide");
     emailInputGroup.classList.remove("input-group-error");
+  });
 });
-
-
+// HAMBURGER AND CLOSE MENU SECTION
 
 // export default app;
 
